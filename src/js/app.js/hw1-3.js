45f6275dbedea2ca9b51cc20b9ba20853949c2b6
Natalia Ryzhova <natashta@gmail.com>
2019-01-21 23:07:35 +0300

    Update hw1-3.js

--- src/js/app.js/hw1-3.js
+++ src/js/app.js/hw1-3.js
@@ -1,9 +1,9 @@
 'use strict'
-let arr = [74989, 74990, 74990, 62000, 58480, 61800];
+var arr = [74989, 74990, 74990, 62000, 58480, 61800];
 
-let sorted = arr.sort(function(a,b){return b-a;});
-const max = sorted[0];
-const middle = (sorted[0] + sorted[1] + sorted[2])/3;
+var sorted = arr.sort(function(a,b){return b-a;});
+var max = sorted[0];
+var middle = sorted.slice(0, 3).reduce((a, b) => a + b, 0)/3;
 
 console.log(max);
 console.log(middle);
