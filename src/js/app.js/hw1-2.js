8ce6a6e926638b3cf992f2dcb612044383f5b479
Natalia Ryzhova <natashta@gmail.com>
2019-01-21 09:30:52 +0300

    Create hw1-2.js

---
+++ src/js/app.js/hw1-2.js
@@ -0,0 +1,16 @@
+const formatEnding = (balans) => {
+  if ((balans == 11) || (balans == 12) || (balans == 13) || (balans == 14)) {return 'ов';}
+  else {
+let num = balans.toString().slice(-1);
+let ending;
+
+if (num == '1') {return ' ';}
+	else if ((num == '2') || (num == '3') || (num == '4')) {return 'a';}
+	else {return 'ов'};
+  }
+}
+const balans1 = 523;
+const output = `Ваш баланс: ${balans1} балл${formatEnding(balans1)}\n
+Ваш баланс: ${balans2} балл${formatEnding(balans2)}\n
+Ваш баланс: ${balans3} балл${formatEnding(balans3)}`;
+console.log(output); 
