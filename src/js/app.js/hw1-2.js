53ec3488b30ad0877a654edb547d4e9987e83d44
Natalia Ryzhova <natashta@gmail.com>
2019-01-22 15:49:23 +0300

    Update hw1-2.js

--- src/js/app.js/hw1-2.js
+++ src/js/app.js/hw1-2.js
@@ -1,18 +1,23 @@
-const formatEnding = (balans) => {
-  if ((balans == 11) || (balans == 12) || (balans == 13) || (balans == 14)) {return 'ов';}
+const formatEnding = (strings, balans) => {
+const string1 = strings[0]; 
+const string2 = strings[1]; 
+let ending;
+  if ((balans == 11) || (balans == 12) || (balans == 13) || (balans == 14)) {ending = 'ов';}
   else {
-	let num = balans.toString().slice(-1);
-	let ending;
+let num = balans.toString().slice(-1);
 
-	if (num == '1') {return ' ';}
-	else if ((num == '2') || (num == '3') || (num == '4')) {return 'a';}
-	else {return 'ов'};
+if (num == '1') {ending = ' ';}
+	else if ((num == '2') || (num == '3') || (num == '4')) {ending = 'a';}
+	else {ending = 'ов'};
   }
+return `${string1}${balans}${string2}${ending}`
 }
+const end = '';
 const balans1 = 523;
 const balans2 = 6000;
 const balans3 = 5001;
-const output = `Ваш баланс: ${balans1} балл${formatEnding(balans1)}\n
-Ваш баланс: ${balans2} балл${formatEnding(balans2)}\n
-Ваш баланс: ${balans3} балл${formatEnding(balans3)}`;
-console.log(output); 
+const output1 = formatEnding`Ваш баланс: ${balans1} балл${end}`
+const output2 = formatEnding`Ваш баланс: ${balans2} балл${end}`
+const output3= formatEnding`Ваш баланс: ${balans3} балл${end}`;
+
+console.log(output1,"\n", output2,"\n", output3);
